6fed1cf8ff7a67374203a3025d7c7bc502c3d3aa
MaZhiwei <mzw1004@126.com>
2015-04-25 20:02:31 +0800

    change update method

--- app/src/main/java/com/twt/service/wenjin/ui/common/UpdateDialogFragment.java
+++ app/src/main/java/com/twt/service/wenjin/ui/common/UpdateDialogFragment.java
@@ -1,19 +1,15 @@
 package com.twt.service.wenjin.ui.common;
 
 import android.app.Dialog;
-import android.app.DownloadManager;
-import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.twt.service.wenjin.R;
-
-import java.io.FileNotFoundException;
 
 /**
  * Created by M on 2015/4/20.
@@ -38,11 +34,16 @@ public class UpdateDialogFragment extends DialogFragment {
         public void onPositive(MaterialDialog dialog) {
             super.onPositive(dialog);
             String url = getArguments().getString(PARAM_UPDATE_URL);
-            DownloadManager downloadManager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
-            Uri uri = Uri.parse(url);
-            DownloadManager.Request request = new DownloadManager.Request(uri);
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-            downloadManager.enqueue(request);
+//            DownloadManager downloadManager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
+//            Uri uri = Uri.parse(url);
+//            DownloadManager.Request request = new DownloadManager.Request(uri);
+//            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+//            downloadManager.enqueue(request);
+            Intent intent = new Intent();
+            intent.setAction("android.intent.action.VIEW");
+            Uri content_url = Uri.parse(url);
+            intent.setData(content_url);
+            startActivity(intent);
         }
 
     }
